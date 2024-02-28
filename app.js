b06fa6379393557e3ba0a14390c2ad1cacf3c4c9
Uday4501 <21311a0573@sreenidhi.edu.in>
2024-02-28 23:16:15 +0530

    database connected2

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ let name1="";
 let name2="";
 let answer="START";
 let answerImage="/images/"+answer+".png";
-const PORT=process.env.PORT || 3000;;
+const PORT=process.env.PORT;
 const app=express();
 
 app.use(bodyParser.urlencoded({extended:true}));
